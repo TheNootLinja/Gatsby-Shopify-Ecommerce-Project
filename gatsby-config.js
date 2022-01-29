72aaf072bd6197ea0a937c156b2797c460f487a2
TheNootLinja <thenootlinja@gmail.com>
2022-01-29 18:05:43 -0500

    Setup the products.js page and the query for all products query to shopify api

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,18 @@
-module.exports = {
-    siteMetadata: {
-      title: `sendnoods-ecommerce`,
-        siteUrl: `https://www.yourdomain.tld`,
-    },
-    plugins: [
+require('dotenv').config();
 
-    ]
-}
+module.exports = {
+  siteMetadata: {
+    title: `sendnoods-ecommerce`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-source-shopify',
+      options: {
+        password: process.env.GATSBY_SHOPIFY_SHOP_PASSWORD,
+        storeUrl: process.env.GATSBY_SHOPIFY_STORE_URL,
+      },
+    },
+    'gatsby-plugin-image',
+  ],
+};
