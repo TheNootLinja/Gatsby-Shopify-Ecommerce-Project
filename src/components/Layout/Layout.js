69f7103d5e87708c189e9980817b3ed0c46c91a6
TheNootLinja <thenootlinja@gmail.com>
2022-01-30 15:43:24 -0500

    Add styling to html/body to remove unwanted margins and begin styling the navbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import { contentWrapper } from './Layout.module.css';
+import { siteTitle, wrapper, contentWrapper, linkItem, navLinks, navBar } from './Layout.module.css';
+import '../../global.css'
 
 const Layout = ({ children }) => {
   return (
-  <div className="wrapper">
-      <nav>
-          <ul>
-            <li>
+  <div className={wrapper}>
+      <nav className={navBar}>
+      <header className={siteTitle}>Send Noods</header>
+          <ul className={navLinks}>
+            <li className={linkItem}>
               <Link to="/">
                 Noods Home
               </Link>
             </li>
-            <li>
+            <li className={linkItem}>
               <Link to="/products">
                 The Noods
               </Link>
             </li>
-            <li>
+            <li className={linkItem}>
               <Link to="/about">
                 About Noods
               </Link>
---
+++ src/templates/product.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const product = ({ pageContext }) => {
+  return <div></div>;
+};
+
+export default product;
