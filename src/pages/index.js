bcacbe6994bb014142c6fd5ca3a4d86f05698e56
TheNootLinja <thenootlinja@gmail.com>
2022-02-06 20:19:24 -0500

    Add some test styling to test combinations of colors

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {homePage, homePageBackground} from './index.module.css'
+import {homePage, homePageBackground, mainContentContainer, sendNoods, sendNoodsUnder, mainText} from './index.module.css'
 import Layout from '../components/Layout/Layout';
 import PastasImg from '../images/pastas.jpg'
 
@@ -8,7 +8,11 @@ const IndexPage = () => {
     <Layout>
       <img src={PastasImg} className={homePageBackground} />
       <div className={homePage}>
-        <p>Still Here!</p>
+        <div className={mainContentContainer}>
+          <h1 className={sendNoods}>Send Noods?</h1>
+          <h2 className={sendNoodsUnder}>That's what we do!</h2>
+          <p className={mainText}>From Alfabeto to Ziti, we are your one stop shop for all of your pasta needs. We even carry specialy ramen cups that we really think you’ll love!</p>
+        </div>
       </div>
     </Layout>
   )
