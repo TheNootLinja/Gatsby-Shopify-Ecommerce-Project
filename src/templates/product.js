7f62226b57f4b7616bc6723dcfcef0b5cc91c0c4
TheNootLinja <thenootlinja@gmail.com>
2022-02-07 21:35:26 -0500

    Add picture of product on individual product page

--- src/templates/product.js
+++ src/templates/product.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Layout from '../components/Layout/Layout';
+import { productImg } from './product.module.css';
 
 const ProductTemplate = ({ pageContext }) => {
   const { product } = pageContext;
+  console.log(product.images[0])
   return (
   <Layout>
     <h1>{product.title}</h1>
+    <img className={productImg} src={product.images[0]?.originalSrc} alt="" />
     <div>{product.description}</div>
+    <label htmlFor="qty">Quantity: </label>
+    <input placeholder="1" id="qty" type="number" defaultValue={1} />
+    <button onClick={() => alert("Added to Cart!")}>Add to Cart</button>
   </Layout>
   )
 };
