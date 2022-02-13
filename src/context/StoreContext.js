f77f56849911d11e477faf03202820f5fe40fbd5
TheNootLinja <thenootlinja@gmail.com>
2022-02-13 16:04:17 -0500

    Add functions for adding and removing items from the shopping cart state and shopify cart

--- src/context/StoreContext.js
+++ src/context/StoreContext.js
@@ -7,6 +7,7 @@ const cleint = Client.buildClient({
   storefrontAccessToken: process.env.GATSBY_STOREFRONT_ACCESS_TOKEN
 }, fetch)
 
+// Defining the default value used in context
 const defaultValues = {
   cart: [],
   loading: false,
@@ -39,6 +40,7 @@ export const StoreProvider = ({ children }) => {
     if(isBrowser) {
       localStorage.setItem(localStorageKey, checkout.id)
     }
+    
     setCheckout(checkout);
   }
 
@@ -72,5 +74,124 @@ export const StoreProvider = ({ children }) => {
     initializeCheckout()
 
   }, [])
+
+  // Defining function for adding item to cart and passing in product and quantity
+  const addVariantToCart = async (product, quantity) => {
+    // Setting loading to true
+    setLoading(true);
+    // Checking if the checkout id is blank
+    if(checkout.id === '') {
+      // Logging error and then returning from function
+      console.error('No checkout ID has been assigned!');
+      return;
+    }
+    // If checkout id isn't blank:
+    // Store value in a variable
+    const checkoutID = checkout.id;
+    // Storing varaints shopify id in a variable
+    const variantId = product.variants[0]?.shopifyId;
+    // Storing value of parseInt function with quantity and base 10 as arguments passed
+    const parsedQuantity = parseInt(quantity, 10);
+    // Add object for item to lineItemsToUpdate array
+    const lineItemsToUpdate = [
+      {
+        variantId,
+        quantity: parsedQuantity,
+      },
+    ]
+
+    // Start a try block
+    try {
+      // Using the addLineItems shopify function with checkoutID and lineItemsToUpdate as arguments
+      const res = await client.checkout.addLineItems(checkoutID, lineItemsToUpdate);
+      // Setting the checkout to the response from addLineItems function
+      setCheckout(res);
+      // Initializing empty array for updatedCart variable
+      let updatedCart = [];
+      // Checking if there are any items in the cart
+      if(cart.length > 0) {
+        // If there is, check if the item we are trying to add is already in the array
+        const itemIsInCart = cart.find((item) => item.product.variants[0]?.shopifyId === variantId);
+        // Check if item was found to already be in the array
+        if(itemIsInCart) {
+          // If it is initialize newProduct variable which will be an object where the
+          // product is xxx and the quantity is the previous quantity + the newly added quantity
+          const newProduct = {
+            product: { ...itemIsInCart.product },
+            quantity: (itemIsInCart.quantity + parsedQuantity)
+          }
+          // Filtering the current item out of the cart so we can get all of the other items
+          const otherItems = cart.filter((item) => item.product.variants[0]?.shopifyId !== variantId);
+          // Spreading the previous items of the cart along with the new product into the
+          // updatedCart variable
+          updatedCart = [...otherItems, newProduct];
+          // If item was found to not be in the cart already
+        } else {
+          // Set updatedCart to the contents of cart with the new item added as the quantity
+          // won't need to be updated for said product
+          updatedCart = cart.concat([{ product, quantity: parsedQuantity }])
+        }
+      }
+      // If there is nothing currently in the cart
+      else {
+        // Set updatedCart to be the new product we want to add
+        updatedCart = [{ product, quantity: parsedQuantity }]
+      }
+      // Use the setCart function with the updatedCart variable as an argument to update the cart state
+      setCart(updatedCart);
+      // Set loading state to false
+      setLoading(false);
+      // Throw up an alert to let user know item has been added to the cart
+      alert('Item added to cart!');
+      // If there is an error
+    } catch(error) {
+      // Set loading state to false
+      setLoading(false);
+      // Log the error to the console with the error object
+      console.error(`Error in addVariantToCart: ${error}`)
+    }
+
+  };
+
+  // Defining function for removing item from cart and passing in the variantId as argument
+  const removeLineItem = async (variantId) => {
+    // Set loading state to false
+    setLoading(true);
+    // Enter try block
+    try {
+      // Initializing lineItemID variable as empty string
+      let lineItemID = '';
+      // Going through each item in the checkout looking for the item the user
+      // wants to remove
+      checkout.lineItems?.forEach((item) => {
+        // If the item is found set lineItemID to the items id
+        if(item.variableValues.lineItems[0]?.variantId === variantId) {
+          lineItemID = item.id
+        };
+      });
+      // If the item is not found log message indicating this to the console and return
+      if(!lineItemID) {
+        console.log('Product not in cart')
+        return
+      };
+      // Use the removeLineItems shopify function with checkout id and an array with
+      // the id of the line item to remove
+      const res = await client.checkout.removeLineItems(checkout.id, [lineItemID])
+      // Setting the checkout state to the response from shopify function
+      setCheckout(res)
+      // Setting updatedCart variable to contents of cart with desired item removed
+      const updatedCart = cart.filter((item) => item.product.variants[0]?.shopifyId !== variantId)
+      // Setting cart state to the updateCart variable
+      setCart(updatedCart)
+      // Set loading state to false
+      setLoading(false)
+      // Catching if there is an error
+    } catch (error) {
+      // Set loading state to false
+      setLoading(false)
+      // Log error to console with error object
+      console.error(`Error in removeLineItem: ${error}`)
+    }
+  }
 
 }
